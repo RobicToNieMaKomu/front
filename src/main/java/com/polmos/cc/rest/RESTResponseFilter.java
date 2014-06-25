5e1e04bad72350eebe9987c4c13e3c159bf0df33
RobicToNieMaKomu <IbetYouWould@know.it>
2014-06-25 21:16:20 +0200

    .

--- src/main/java/com/polmos/cc/rest/RESTResponseFilter.java
+++ src/main/java/com/polmos/cc/rest/RESTResponseFilter.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerResponseContext;
 import javax.ws.rs.container.ContainerResponseFilter;
-import javax.ws.rs.container.PreMatching;
+//import javax.ws.rs.container.PreMatching;
 import javax.ws.rs.ext.Provider;
 import org.jboss.logging.Logger;
 
@@ -13,11 +13,15 @@ import org.jboss.logging.Logger;
  * @author RobicToNieMaKomu
  */
 @Provider
-@PreMatching
+//@PreMatching
 public class RESTResponseFilter implements ContainerResponseFilter {
 
     private static final Logger logger = Logger.getLogger(RESTResponseFilter.class);
 
+    public RESTResponseFilter() {
+        logger.info("RESTResponseFilter constructed");
+    }
+    
     @Override
     public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) throws IOException {
         logger.info("Adding additional headers to the response");
