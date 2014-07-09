4dae22805bb797aea804cded5892095e395c1bd0
RobicToNieMaKomu <IbetYouWould@know.it>
2014-07-09 21:36:23 +0200

    .

--- src/main/webapp/assets/js/info.js
+++ src/main/webapp/assets/js/info.js
@@ -1,6 +1,5 @@
 function Info(about) {
     var about = about;
-    var urlToImg = 'http://front-comparator.rhc.cloud.com/';
     this.getTitle = function() {
         var title = '';
         if (about === 'about') {
@@ -13,7 +12,11 @@ function Info(about) {
     this.getMessage = function() {
         var msg = '';
         if (about === 'about') {
-            msg = '<div><p>This is about content.</p></div>';
+            msg += '<div><p>';;
+            msg += 'Graphs are constructed based on real data that comes from NY Forex. ';
+            msg += 'Application polls data in 10s intervals, so refreshing site faster than that won\'t take any effect.<br/><br/>';
+            msg += 'Author doesn\'t pretend to be front end jedi (he feels much better when server is near though), so leave your silly jokes for someone else:) ';
+            msg += '</p></div>';
         } else if (about === 'contact') {
             msg += '<div>';
             msg += '<p>If you have any questions or comments regarding this site or myself do not hesitate and write me an email:</p></hr>'; 
@@ -21,7 +24,6 @@ function Info(about) {
             msg += '<p>or visit my linked-in profile:<p></hr>';
             msg += '<p><span class="fa fa-linkedin"></span> <a href="http://pl.linkedin.com/pub/piotr-szcz%C4%99%C5%9Bniak/77/597/489">Linkedin profile </a></p>';
             msg += '</div>';
-            //<img src=\'assets/images/gmail.png\'><span class="glyphicon glyphicon-info-sign"></span>
         }
         return msg;
     };
